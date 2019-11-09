31cbb28d75e4fb955bfa423a05b47afc889a6059
Chad Lubrecht <chad@calubrecht.us>
2019-11-09 12:09:18 -0500

    Mix session and local storage, so uses can have 2 logins on same browser

--- src/app/storage.service.ts
+++ src/app/storage.service.ts
@@ -12,15 +12,23 @@ export class StorageService {
   setToken(token)
   {
     localStorage.setItem('TOKEN', token);
+    sessionStorage.setItem('TOKEN', token);
   }
+
   getToken()
   {
-    return localStorage.getItem('TOKEN');
+    let token = sessionStorage.getItem('TOKEN');
+    if (!token)
+    {
+      token = localStorage.getItem('TOKEN');
+    }
+    return token;
   }
 
   clearToken()
   {
     localStorage.removeItem('TOKEN');
+    sessionStorage.removeItem('TOKEN');
   }
 
   setName(name: string)
