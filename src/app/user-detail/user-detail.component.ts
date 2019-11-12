d086fa22d0393907a9b35599d1e70e94f9a4d11a
Chad Lubrecht <chad@calubrecht.us>
2019-11-11 20:24:04 -0500

    Styling with gradient and rounded borders. do no display invite button for yourself

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,5 +1,6 @@
 import { Input, Component, OnInit } from '@angular/core';
 import { UserData } from '../user-data';
+import { UserLoginService } from '../user-login.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,7 +9,7 @@ import { UserData } from '../user-data';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(public loginService : UserLoginService) { }
 
   ngOnInit() {
   }
