8f0c623a38ab5818a7c47e7dd31d04c2ad2f1b00
Chad Lubrecht <chad@calubrecht.us>
2022-06-30 00:31:16 -0400

    test RegisterComponent

---
+++ src/app/register/register.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { RegisterComponent } from './register.component';
+
+@Component({
+  selector: 'app-msg',
+  template: '<p>Msg</p>'
+})
+class MockMsg {}
+
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule, FormsModule],
+      declarations: [ RegisterComponent, MockMsg ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
