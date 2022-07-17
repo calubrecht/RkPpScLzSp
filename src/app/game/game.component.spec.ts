17f44684cf600b0ec702d4483447c4853110e266
Chad Lubrecht <chad@calubrecht.us>
2022-07-17 13:52:06 -0400

    Adding game component test

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { GameMessage, GameService } from '../game.service';
 
 import { GameComponent } from './game.component';
 
@@ -11,6 +12,17 @@ import { GameComponent } from './game.component';
 })
 class MockMsg {}
 
+let mockListen = jasmine.createSpy("listen");
+
+class MockGame {
+  gameStatus = {gameStatus: 'active'};
+  listen = mockListen;
+}
+class MockGameFinished {
+  gameStatus = {gameStatus: 'finished'};
+  listen = mockListen;
+}
+
 describe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
@@ -19,6 +31,9 @@ describe('GameComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ GameComponent, MockMsg ],
       imports: [RouterTestingModule, HttpClientModule],
+      providers: [
+        {provide: GameService, useClass: MockGame}
+      ],
     })
     .compileComponents();
 
@@ -29,5 +44,32 @@ describe('GameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(fixture.debugElement.nativeElement.getElementsByTagName('button')[0].innerText).toBe('Cancel Game');
+    expect(mockListen).toHaveBeenCalled();
+  });
+});
+
+describe('GameComponent Finished', () => {
+  let component: GameComponent;
+  let fixture: ComponentFixture<GameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GameComponent, MockMsg ],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [
+        {provide: GameService, useClass: MockGameFinished}
+      ],
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+    expect(fixture.debugElement.nativeElement.getElementsByTagName('button')[0].innerText).toBe('Close Game');
   });
 });
