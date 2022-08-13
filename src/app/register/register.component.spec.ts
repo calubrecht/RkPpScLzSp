b7f0a96f5f4ddacc8f27df121888d6b062cf3c6a
Chad Lubrecht <chad@calubrecht.us>
2022-08-13 15:50:25 -0400

    Finish register component tests

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -18,6 +18,7 @@ class MockMsg {}
 
 let routerSpy;
 let loginSpy;
+let msgSpy;
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -26,12 +27,13 @@ describe('RegisterComponent', () => {
   beforeEach(async () => {
     routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
     loginSpy = jasmine.createSpyObj('UserLoginService', ['register', 'isLoggedIn']);
+    msgSpy = jasmine.createSpyObj('MsgService', ['setError']);
     await TestBed.configureTestingModule({
       imports: [HttpClientModule, FormsModule],
       declarations: [ RegisterComponent, MockMsg ],
       providers: [
         { provide:UserLoginService, useValue:loginSpy},
-        MockProvider(MsgService),
+        { provide:MsgService, useValue:msgSpy},
         { provide: Router, useValue: routerSpy}
       ]
     })
@@ -64,5 +66,83 @@ describe('RegisterComponent', () => {
 
     expect(loginSpy.register).toHaveBeenCalled();
     expect(loginSpy.register).toHaveBeenCalledWith('loser1', 'pass1', 'red');
+  });
+  
+  it('should cancel', () => {
+
+    let cancelButton = fixture.nativeElement.querySelector("#cancelBtn");
+    cancelButton.click();
+
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('login');
+  });
+  
+  it('should validate', () => {
+
+    let userNameInput = fixture.nativeElement.querySelector("#userName");
+    let passwordInput = fixture.nativeElement.querySelector("#password");
+    let passwordConfirmInput = fixture.nativeElement.querySelector("#confirmPassword");
+    userNameInput.value = "loser1";
+    passwordInput.value = "pass1";
+    passwordConfirmInput.value = "pass1";
+    userNameInput.dispatchEvent(new Event("input"));
+    passwordInput.dispatchEvent(new Event("input"));
+    passwordConfirmInput.dispatchEvent(new Event("input"));
+    fixture.detectChanges();
+    
+    expect(component.validate()).toBe(true);
+    
+    component.userName = "";
+    expect(component.validate()).toBe(false);
+    expect(msgSpy.setError).toHaveBeenCalledWith('Please enter a username');
+    msgSpy.setError.calls.reset();
+    expect(msgSpy.setError).not.toHaveBeenCalled();
+    component.userName = null;
+    expect(component.validate()).toBe(false);
+    expect(msgSpy.setError).toHaveBeenCalledWith('Please enter a username');
+    msgSpy.setError.calls.reset();
+    component.userName = "user";
+    component.password = "";
+    expect(component.validate()).toBe(false);
+    expect(msgSpy.setError).toHaveBeenCalledWith('Please enter a password');
+    msgSpy.setError.calls.reset();
+    component.password = null;
+    expect(component.validate()).toBe(false);
+    expect(msgSpy.setError).toHaveBeenCalledWith('Please enter a password');
+    msgSpy.setError.calls.reset();
+    component.password = "pass1";
+    component.confirmPassword = "pass9";
+    expect(component.validate()).toBe(false);
+    expect(msgSpy.setError).toHaveBeenCalledWith('Password confirmation does not match');
+  });
+});
+
+describe('RegisterComponent loggedinAlready', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+
+  beforeEach(async () => {
+    routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+    loginSpy = jasmine.createSpyObj('UserLoginService', {
+      'isLoggedIn': true});
+    msgSpy = jasmine.createSpyObj('MsgService', ['setError']);
+    await TestBed.configureTestingModule({
+      imports: [HttpClientModule, FormsModule],
+      declarations: [ RegisterComponent, MockMsg ],
+      providers: [
+        { provide:UserLoginService, useValue:loginSpy},
+        { provide:MsgService, useValue:msgSpy},
+        { provide: Router, useValue: routerSpy}
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should navigate to lobby', () => {
+    expect(component).toBeTruthy();
+    expect(routerSpy.navigateByUrl).toHaveBeenCalledWith('lobby');
   });
 });
