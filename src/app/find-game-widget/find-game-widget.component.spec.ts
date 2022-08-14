77572a1b92087fd21fbdf08d0e50a51a319bd235
Chad Lubrecht <chad@calubrecht.us>
2022-08-13 22:43:39 -0400

    find game tests

--- src/app/find-game-widget/find-game-widget.component.spec.ts
+++ src/app/find-game-widget/find-game-widget.component.spec.ts
@@ -1,18 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GameService, GameMessage, GameListener } from '../game.service';
 import { HttpClientModule } from '@angular/common/http';
 import { MockProvider, MockService } from 'ng-mocks';
 
 import { FindGameWidgetComponent } from './find-game-widget.component';
+
+let gameSpy;
 
 describe('FindGameWidgetComponent', () => {
   let component: FindGameWidgetComponent;
   let fixture: ComponentFixture<FindGameWidgetComponent>;
 
   beforeEach(async () => {
+    gameSpy = jasmine.createSpyObj('GameService', ['seekGame', 'onInit', 'listen'], {gameStatus: {inGame:false}});
     await TestBed.configureTestingModule({
       declarations: [FindGameWidgetComponent ],
       imports: [RouterTestingModule, HttpClientModule],
+      providers: [
+        { provide:GameService, useValue:gameSpy}]
     })
     .compileComponents();
 
@@ -23,5 +29,20 @@ describe('FindGameWidgetComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('does Seek', () => {
+    let seekButton = fixture.nativeElement.querySelector("#seekGameBtn");
+    seekButton.click();
+
+    expect(component.isSeeking).toBe(true);
+    expect(component.seekingLongTime).toBe(false);
+    expect(gameSpy.seekGame).toHaveBeenCalled();
+    let seekingTimer = component.seekingTimer;
+
+    // Second seek resets timer
+    seekButton.click();
+    expect(component.seekingTimer).not.toBe(seekingTimer)
+
   });
 });
